18ceb7f4cc337288af2f042bdd81fd9c5cc3490e
tobias.mikha <tobias.sulistiyo@indotaichen.com>
2026-02-13 16:29:45 +0700

    migrate:(HapusDataKPE) Migrasiin hapus data kpe

--- pages/hapusdatakpe.php
+++ pages/hapusdatakpe.php
@@ -2,10 +2,21 @@
 ini_set("error_reporting", 1);
 session_start();
 include("../koneksi.php");
-    $modal_id=$_GET['id'];
-    $modal=mysqli_query($con,"DELETE FROM tbl_aftersales_now WHERE id='$modal_id' ");
-    if ($modal) {
-        echo "<script>window.location='./LapKPE';</script>";
-    } else {
-        echo "<script>alert('Gagal Hapus');window.location='./LapKPE';</script>";
-    }
+
+$modal_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+$sql = "DELETE FROM db_qc.tbl_aftersales_now WHERE id = ?";
+$params = [$modal_id];
+
+$modal = sqlsrv_query($con_db_qc_sqlsrv, $sql, $params);
+
+if ($modal === false) {
+    echo "<pre>";
+    print_r(sqlsrv_errors());
+    echo "</pre>";
+    echo "<script>alert('Gagal Hapus');window.location='./LapKPE';</script>";
+    exit;
+}
+
+echo "<script>window.location='./LapKPE';</script>";
+?>
