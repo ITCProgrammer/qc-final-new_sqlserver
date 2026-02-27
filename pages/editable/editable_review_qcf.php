64b481b39ed54c5768498b0c20471e9cac4d236e
Nktpamungkas <nktpamungkas28@gmail.com>
2026-02-27 20:03:03 +0700

    migrasi: (editable) Migrasikan file yg kelewat

--- pages/editable/editable_review_qcf.php
+++ pages/editable/editable_review_qcf.php
@@ -3,6 +3,17 @@ ini_set("error_reporting", 1);
 session_start();
 include "../../koneksi.php";
 
-mysqli_query($con,"UPDATE tbl_lap_inspeksi SET `review_qcf` = '$_POST[value]' where id = '$_POST[pk]'");
+$value = isset($_POST['value']) ? $_POST['value'] : null;
+$pk = isset($_POST['pk']) ? $_POST['pk'] : null;
+
+$sql = "UPDATE db_qc.tbl_lap_inspeksi SET review_qcf = ? WHERE id = ?";
+$params = array($value, $pk);
+$stmt = sqlsrv_query($con_db_qc_sqlsrv, $sql, $params);
+
+if ($stmt === false) {
+	http_response_code(500);
+	echo json_encode('failed');
+	exit;
+}
 
 echo json_encode('success');
