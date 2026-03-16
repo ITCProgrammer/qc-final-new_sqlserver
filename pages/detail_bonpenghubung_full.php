ff15b4e8f5826ac3849d6e7741b159c92f769565
deka0305 <dedenkurnia35@gmail.com>
2026-03-16 09:29:06 +0700

    migrasi tampilan cetak Detail Bon Penghubung perubahan query dan normalisasi data

--- pages/detail_bonpenghubung_full.php
+++ pages/detail_bonpenghubung_full.php
@@ -5,6 +5,13 @@ include("../koneksi.php");
 $no_po = isset($_GET['no_po']) ? $_GET['no_po'] : '';
 $no_hanger = isset($_GET['no_hanger']) ? $_GET['no_hanger'] : '';
 $no_warna = isset($_GET['no_warna']) ? $_GET['no_warna'] : '';
+
+if (!function_exists('bp_value')) {
+    function bp_value($value, $default = '')
+    {
+        return ($value === null || $value === '') ? $default : $value;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -54,75 +61,114 @@ $no_warna = isset($_GET['no_warna']) ? $_GET['no_warna'] : '';
             $total_panjang_estimasi = 0;
             $sqldtl = "SELECT
                 tq.*,
-                GROUP_CONCAT(DISTINCT b.no_ncp_gabungan SEPARATOR ', ') AS no_ncp,
-                GROUP_CONCAT(DISTINCT b.masalah_dominan SEPARATOR ', ') AS masalah_utama,
-                GROUP_CONCAT(DISTINCT b.akar_masalah SEPARATOR ', ') AS akar_masalah,
-                GROUP_CONCAT(DISTINCT b.solusi_panjang SEPARATOR ', ') AS solusi_panjang,
-                tli.qty_loss AS qty_sisa,
-                tli.satuan AS satuan_sisa,
+                ncp.no_ncp,
+                ncp.masalah_utama,
+                ncp.akar_masalah,
+                ncp.solusi_panjang,
+                tli.qty_sisa,
+                tli.satuan_sisa,
                 c.masalah_dominan,
                 c.ket
             FROM
-                tbl_qcf tq
-                LEFT JOIN tbl_lap_inspeksi tli ON tq.nodemand = tli.nodemand AND tq.no_order = tli.no_order
-                LEFT JOIN tbl_ncp_qcf_now b ON tq.nodemand = b.nodemand
-                LEFT JOIN tbl_aftersales_now c ON c.nodemand = tq.nodemand AND c.nokk = tq.nokk
+                db_qc.tbl_qcf tq
+                OUTER APPLY (
+                    SELECT
+                        STUFF((
+                            SELECT DISTINCT ', ' + CAST(b1.no_ncp_gabungan AS VARCHAR(MAX))
+                            FROM db_qc.tbl_ncp_qcf_now b1
+                            WHERE b1.nodemand = tq.nodemand
+                                AND ISNULL(CAST(b1.no_ncp_gabungan AS VARCHAR(MAX)), '') <> ''
+                            FOR XML PATH(''), TYPE
+                        ).value('.', 'VARCHAR(MAX)'), 1, 2, '') AS no_ncp,
+                        STUFF((
+                            SELECT DISTINCT ', ' + CAST(b2.masalah_dominan AS VARCHAR(MAX))
+                            FROM db_qc.tbl_ncp_qcf_now b2
+                            WHERE b2.nodemand = tq.nodemand
+                                AND ISNULL(CAST(b2.masalah_dominan AS VARCHAR(MAX)), '') <> ''
+                            FOR XML PATH(''), TYPE
+                        ).value('.', 'VARCHAR(MAX)'), 1, 2, '') AS masalah_utama,
+                        STUFF((
+                            SELECT DISTINCT ', ' + CAST(b3.akar_masalah AS VARCHAR(MAX))
+                            FROM db_qc.tbl_ncp_qcf_now b3
+                            WHERE b3.nodemand = tq.nodemand
+                                AND ISNULL(CAST(b3.akar_masalah AS VARCHAR(MAX)), '') <> ''
+                            FOR XML PATH(''), TYPE
+                        ).value('.', 'VARCHAR(MAX)'), 1, 2, '') AS akar_masalah,
+                        STUFF((
+                            SELECT DISTINCT ', ' + CAST(b4.solusi_panjang AS VARCHAR(MAX))
+                            FROM db_qc.tbl_ncp_qcf_now b4
+                            WHERE b4.nodemand = tq.nodemand
+                                AND ISNULL(CAST(b4.solusi_panjang AS VARCHAR(MAX)), '') <> ''
+                            FOR XML PATH(''), TYPE
+                        ).value('.', 'VARCHAR(MAX)'), 1, 2, '') AS solusi_panjang
+                ) ncp
+                OUTER APPLY (
+                    SELECT TOP 1
+                        qty_loss AS qty_sisa,
+                        satuan AS satuan_sisa
+                    FROM db_qc.tbl_lap_inspeksi
+                    WHERE nodemand = tq.nodemand
+                        AND no_order = tq.no_order
+                ) tli
+                OUTER APPLY (
+                    SELECT TOP 1
+                        masalah_dominan,
+                        ket
+                    FROM db_qc.tbl_aftersales_now
+                    WHERE nodemand = tq.nodemand
+                        AND nokk = tq.nokk
+                ) c
             WHERE
-                tq.sts_pbon != '10'
+                ISNULL(CAST(tq.sts_pbon AS VARCHAR(50)), '') != '10'
                 AND (
-                    tq.penghubung_masalah != ''
-                    OR tq.penghubung_keterangan != ''
-                    OR tq.penghubung_roll1 != ''
-                    OR tq.penghubung_roll2 != ''
-                    OR tq.penghubung_roll3 != ''
-                    OR tq.penghubung_dep != ''
-                    OR tq.penghubung_dep_persen != ''
+                    ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_masalah AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_keterangan AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_roll1 AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_roll2 AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_roll3 AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_dep AS VARCHAR(MAX)))), '') != ''
+                    OR ISNULL(LTRIM(RTRIM(CAST(tq.penghubung_dep_persen AS VARCHAR(MAX)))), '') != ''
                 )
-                AND tq.no_po = '" . mysqli_real_escape_string($con, $no_po) . "'
-                AND tq.no_hanger = '" . mysqli_real_escape_string($con, $no_hanger) . "'
-                AND tq.no_warna = '" . mysqli_real_escape_string($con, $no_warna) . "'
-            GROUP BY
-                tq.no_order,
-                tq.no_po,
-                tq.no_hanger,
-                tq.no_item,
-                tq.warna,
-                tq.pelanggan,
-                tq.tgl_masuk,
-                tq.nodemand
+                AND ISNULL(CAST(tq.no_po AS VARCHAR(255)), '') = ?
+                AND ISNULL(CAST(tq.no_hanger AS VARCHAR(255)), '') = ?
+                AND ISNULL(CAST(tq.no_warna AS VARCHAR(255)), '') = ?
             ";
-            $stmt = mysqli_query($con, $sqldtl);
-            while($r = mysqli_fetch_array($stmt)){
-                $total_berat_order += (float)$r['berat_order'];
-                $total_panjang_order += (float)$r['panjang_order'];
-                $total_estimasi += (float)$r['estimasi'];
-                $total_panjang_estimasi += (float)$r['panjang_estimasi'];
+            $params = array($no_po, $no_hanger, $no_warna);
+            $stmt = sqlsrv_query($con_db_qc_sqlsrv, $sqldtl, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+            while($r = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){
+                $total_berat_order += (float)bp_value($r['berat_order'], 0);
+                $total_panjang_order += (float)bp_value($r['panjang_order'], 0);
+                $total_estimasi += (float)bp_value($r['estimasi'], 0);
+                $total_panjang_estimasi += (float)bp_value($r['panjang_estimasi'], 0);
             ?>
             <tr>
                 <td style="border: 2px solid black;" align="center"><?php echo $no;?></td>
-                <td style="border: 2px solid black;" align="center"><?php $rsts= mysqli_query($con,"SELECT * FROM tbl_bonpenghubung_mail WHERE nodemand='$r[nodemand]' ");
-                    $dtsts = mysqli_fetch_assoc($rsts);
-                    if($dtsts['status_approve']==1){
-                    echo 'APPROVE OLEH : '.$dtsts['approve_mkt'];
-                    }else if($dtsts['status_approve']==99){
-                    echo 'REJECT OLEH : '.$dtsts['approve_mkt'];
-                    }else if($dtsts['status_approve']==2){
-                    echo 'CLOSED OLEH : '.$dtsts['closed_ppc'];
+                <td style="border: 2px solid black;" align="center"><?php $rsts= sqlsrv_query($con_db_qc_sqlsrv, "SELECT TOP 1 * FROM db_qc.tbl_bonpenghubung_mail WHERE nodemand = ?", array(bp_value($r['nodemand'])));
+                    $dtsts = $rsts ? sqlsrv_fetch_array($rsts, SQLSRV_FETCH_ASSOC) : array();
+                    if(isset($dtsts['status_approve']) && $dtsts['status_approve']==1){
+                    echo 'APPROVE OLEH : '.bp_value($dtsts['approve_mkt']);
+                    }else if(isset($dtsts['status_approve']) && $dtsts['status_approve']==99){
+                    echo 'REJECT OLEH : '.bp_value($dtsts['approve_mkt']);
+                    }else if(isset($dtsts['status_approve']) && $dtsts['status_approve']==2){
+                    echo 'CLOSED OLEH : '.bp_value($dtsts['closed_ppc']);
                     } else {
                     echo '';
                     }?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['berat_order'];?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['panjang_order'];?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['estimasi'];?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['panjang_estimasi'];?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['lot_legacy']; // Lot-Legacy ?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['lot']; // Lot ?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['nodemand']; // Demand ?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['penghubung_masalah']; // Issue ?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['penghubung_keterangan']; // Notes ?></td>
-                <td style="border: 2px solid black;" align="center"><?php echo $r['penghubung_dep'].$r['penghubung_dep_persen']; // Responsibility ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['berat_order']);?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['panjang_order']);?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['estimasi']);?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['panjang_estimasi']);?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['lot_legacy']); // Lot-Legacy ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['lot']); // Lot ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['nodemand']); // Demand ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['penghubung_masalah']); // Issue ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['penghubung_keterangan']); // Notes ?></td>
+                <td style="border: 2px solid black;" align="center"><?php echo bp_value($r['penghubung_dep']).bp_value($r['penghubung_dep_persen']); // Responsibility ?></td>
                 <td style="border: 2px solid black;">
-                     <a style="color: #E95D4E; font-size:10px; font-family: Microsoft Sans Serif;" href="cetak/cetak_inspectpackingreport.php?demand=<?= $r['nodemand']; ?>&ispacking=true" target="_blank">Inspect Report <i class="fa fa-link"></i></a>
+                     <a style="color: #E95D4E; font-size:10px; font-family: Microsoft Sans Serif;" href="cetak/cetak_inspectpackingreport.php?demand=<?= urlencode(bp_value($r['nodemand'])); ?>&ispacking=true" target="_blank">Inspect Report <i class="fa fa-link"></i></a>
                 </td>
             </tr>
             <?php $no++;}?>
